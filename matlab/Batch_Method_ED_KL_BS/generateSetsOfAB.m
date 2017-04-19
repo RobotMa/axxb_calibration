8b3061bebab27987be7ef0490527b6807673933d
Qianli Ma <mqianli1@jhu.edu>
2017-04-19 15:05:56 -0400

    minor changes

--- matlab/Batch_Method_ED_KL_BS/generateSetsOfAB.m
+++ matlab/Batch_Method_ED_KL_BS/generateSetsOfAB.m
@@ -1,10 +1,11 @@
-function [A_set, B_set] = generateSetsOfAB( n_sets, num, optPDF, X, gmean, cov)
+function [A_set, B_set] = generateSetsOfAB( n_sets, num, optPDF, ...
+                                            gmean, cov, X, Y)
 
 A_set = zeros(4, 4, num, n_sets);
 B_set = zeros(4, 4, num, n_sets);
 
 for i = 1 : n_sets
-    [A_set(:,:,:,i), B_set(:,:,:,i)] = generateAB(num, optPDF(i), X, gmean, cov);
+    [A_set(:,:,:,i), B_set(:,:,:,i)] = generateAB(num, optPDF, gmean, cov, X, Y);
 end
 
 end
